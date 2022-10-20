a231dd0d7a4c8cfbb0496ee2ec77348cbbd8b48e
Zentai Pál <zentaipali@gmail.com>
2022-10-20 11:44:06 +0200

    kesz feladat

---
+++ app/Http/Controllers/SchoolController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+use App\Models\Course;
+
+
+class SchoolController extends Controller {
+    
+    // public function listStudent() {
+
+    //     $students = Student::with("course")->get();
+
+    //     // foreach ($students as $student ) {
+
+    //     //     echo "<pre>";
+    //     //     print_r($student->name . " ");
+    //     //     print_r($student->course->course);
+    //     // }
+    //     return view("list_student", [
+            
+    //         "students" => $students
+    //     ]);
+    // }
+
+    public function listStudent() {
+
+        $students = Course::find(1)->student;
+
+        return view("list_student", [
+            "students" => $students
+        ]);
+    }
+}
